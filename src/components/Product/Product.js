1245ca616ec76f36d8d3a3a099f7a0879c405ea8
mdsohag65 <sohag92ahmed@gmail.com>
2022-03-25 15:49:43 +0600

    cart added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,30 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Product.css';
+
+// instead of props we can use {product, handleAddtoCart}
+
+const Product = ({ product, handleAddtoCArt }) => {
+    // console.log(props);
+    // const { product, handleAddtoCArt } = props;
+    const { name, img, seller, price, ratings } = product;
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button onClick={() => handleAddtoCArt(product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
